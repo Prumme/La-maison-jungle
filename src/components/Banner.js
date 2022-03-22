56010d33f68cc8cf1902646cfe8d356953e656cf
Prumme <aurelien23.p@gmail.com>
2022-03-22 22:10:27 +0100

    progression dans le cours

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,10 +3,19 @@ import logo from '../assets/logo.png'
 
 function Banner() {
     const title = 'La maison jungle'
+    const currentMonth = new Date().getMonth()
+    const isSpring = currentMonth >= 2 && currentMonth <= 5
+    const recommendation = isSpring ?
+        (<div>C'est le printemp, rempotez 🌱</div>) :
+        (<div>Ce n'est pas le moment de rempoter</div>)
+    console.log(currentMonth)
     return (
         <div className='lmj-banner'>
-            <img src={logo} alt='La maison jungle' className='lmj-logo' />
-            <h1 className='lmj-title'>{title}</h1>
+            <div>
+                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <h1 className='lmj-title'>{title}</h1>
+            </div>
+            {recommendation}
         </div>
     )
 }
